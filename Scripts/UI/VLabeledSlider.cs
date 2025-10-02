7074dbb820dfb20a1de9714f60192ce4e6930095
Nat Fletcher <njfletcher215@gmail.com>
2025-10-02 17:55:51 -0500

    distinguished HLabeledSlider and VLabeledSlider; made tick labels autogenerated

---
+++ Scripts/UI/VLabeledSlider.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+/// <summary>
+/// A mediator between a vertical slider and its title, current value, and tick labels.
+/// </summary>
+public partial class VLabeledSlider : VSlider {
+    /// <summary>
+    /// The simple format string label for the current value of the slider.
+    /// </summary>
+    /// <value>Use "currentValue" in the format string.</value>
+    [Export]
+    private SimpleFormatStringLabel currentValue;
+    [Export]
+    private bool labelTicks;
+
+    private Label[] tickLabels;
+
+    /// <inheritdoc />
+    public override void _Ready() {
+        if (this.labelTicks) this.InitializeTickLabels();
+
+        if (this.currentValue != null) {
+            this.currentValue.SetValue("currentValue", this.Value);
+            this.ValueChanged += (value) =>
+                this.currentValue.SetValue("currentValue", this.Value);
+        }
+    }
+
+    /// <summary>
+    /// Create the tick labels.
+    /// </summary>
+    private void InitializeTickLabels() {
+        this.tickLabels = new Label[this.TickCount];
+
+        double valMin = this.MinValue;
+        double valMax = this.MaxValue;
+        double valOffset = (valMax - valMin) / (this.TickCount - 1);
+
+        float posMin = 0;
+        float posMax = this.Size.Y;
+        float posOffset = (posMax - posMin) / (this.TickCount - 1);
+
+        for (int i = 0; i < this.TickCount; i++) {
+            double val = valMin + (i * valOffset);
+            float pos = posMin + (i * posOffset);
+            Label tickLabel = new Label {
+                Text = val.ToString("0.##"),
+                Position = new Vector2(this.Size.X, pos),
+            };
+
+            // vertically center the tickLabel on pos
+            tickLabel.Position -= new Vector2(0, tickLabel.Size.Y / 2);
+
+            this.tickLabels[i] = tickLabel;
+            this.AddChild(tickLabel);
+        }
+    }
+}
